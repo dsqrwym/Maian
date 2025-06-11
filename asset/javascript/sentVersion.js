49a71e9efee4df4972f4c7bc9c2a44e9c10d37e2
dsqrwym <dsqrwym@gmail.com>
2025-06-11 22:09:40 +0200

    test

---
+++ asset/javascript/sentVersion.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", function () {
+    const meta = document.querySelector('meta[name="version"]');
+    if (meta && meta.content) {
+        window.parent.postMessage(
+            { type: "version", version: meta.content },
+            "*"
+        );
+        console.log("Version sent to parent:", meta.content);
+    }else{
+        console.warn("Version meta tag not found or empty.");
+    }
+});
