64eeefd72d4ba5c7b741cc3b4a10c14caa613871
dsqrwym <dsqrwym@gmail.com>
2025-06-12 00:01:08 +0200

    test

--- asset/javascript/sentVersion.js
+++ asset/javascript/sentVersion.js
@@ -1,15 +1,17 @@
 window.addEventListener("load", function () {
-    const meta = document.querySelector('meta[name="version"]');
-    if (meta && meta.content) {
-        window.parent.postMessage(
-            { type: "version", version: meta.content },
-            "*"
-        );
-        console.log("Version sent to parent:", meta.content);
-        window.postMessage(
-            { type: "version", version: meta.content },
-            "*"
-        );
-        console.log("Version sent to self:", meta.content);
-    }
+    this.setInterval(() => {
+        const meta = document.querySelector('meta[name="version"]');
+        if (meta && meta.content) {
+            window.parent.postMessage(
+                { type: "version", version: meta.content },
+                "*"
+            );
+            console.log("Version sent to parent:", meta.content);
+            window.postMessage(
+                { type: "version", version: meta.content },
+                "*"
+            );
+            console.log("Version sent to self:", meta.content);
+        }
+    }, 1000);
 });
