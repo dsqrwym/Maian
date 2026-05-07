7a481ad307a063ea2e7a3d71c28f6a2c54b5302c
dsqrwym <dsqrwym@gmail.com>
2026-05-07 21:22:18 +0200

    fix(Frontend|StringResource): fix error multilanguage text and error on layout SharedReadolyProduct

--- Frontend/Maian/shared/src/commonMain/kotlin/org/dsqrwym/shared/ui/components/product/detail/SharedProductDetailContent.kt
+++ Frontend/Maian/shared/src/commonMain/kotlin/org/dsqrwym/shared/ui/components/product/detail/SharedProductDetailContent.kt
@@ -4,7 +4,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
@@ -19,8 +36,23 @@ import androidx.compose.material.icons.automirrored.outlined.ArrowForward
 import androidx.compose.material.icons.outlined.HideImage
 import androidx.compose.material.icons.outlined.Store
 import androidx.compose.material.icons.outlined._1xMobiledata
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,7 +71,20 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import maian.shared.generated.resources.*
+import maian.shared.generated.resources.SharedRes
+import maian.shared.generated.resources.available_stock_value
+import maian.shared.generated.resources.description
+import maian.shared.generated.resources.image_content_description
+import maian.shared.generated.resources.max_purchasable_qty_value
+import maian.shared.generated.resources.not_purchasable
+import maian.shared.generated.resources.product_code
+import maian.shared.generated.resources.product_image_with_name
+import maian.shared.generated.resources.product_min_order_qty
+import maian.shared.generated.resources.product_price_with_vat_value
+import maian.shared.generated.resources.product_price_without_vat_value
+import maian.shared.generated.resources.sale_unit_qty_value
+import maian.shared.generated.resources.tax_rate
+import maian.shared.generated.resources.variants
 import org.dsqrwym.shared.data.products.SharedProductSaleVariant
 import org.dsqrwym.shared.data.products.displayName
 import org.dsqrwym.shared.domain.product.SharedProductDetailMedia
@@ -154,7 +199,7 @@ private fun SharedProductMediaInfoLayout(
     onMediaClick: (SharedProductDetailMedia) -> Unit,
 ) {
     BoxWithConstraints (modifier = Modifier.fillMaxSize()){
-        val useTwoColumns = getWidthSizeClass(constraints.maxWidth.dp) != WindowWidthSizeClass.Compact
+        val useTwoColumns = getWidthSizeClass(maxWidth) != WindowWidthSizeClass.Compact
 
         if (useTwoColumns) {
             Row(
