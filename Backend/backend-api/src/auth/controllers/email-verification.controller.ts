9ccfba98d53d5c1f04b13befb3128a5ad407feb5
dsqrwym <dsqrwym@gmail.com>
2026-05-31 12:11:03 +0200

    fix(backend|verification email): return incorrect html

--- Backend/backend-api/src/auth/controllers/email-verification.controller.ts
+++ Backend/backend-api/src/auth/controllers/email-verification.controller.ts
@@ -4,6 +4,7 @@ import { IVerifyEmailQueryDto } from '../dto/verification.dto.js';
 import { VERIFY_EMAIL_PATH } from '../auth.constants.js';
 import { FastifyReply } from 'fastify';
 import { TypedQuery, TypedRoute } from '@nestia/core';
+import { SkipResponseInterceptor } from '#/common/guards/decorator/skip-response-interceptor.decorator.js';
 
 /**
  * Controller for email verification via link
@@ -22,6 +23,7 @@ export class EmailVerificationController {
    * @param {IVerifyEmailQueryDto} query - Contains verification_id and token
    * @param {FastifyReply} reply - Response object for redirect
    */
+  @SkipResponseInterceptor()
   @TypedRoute.Get(VERIFY_EMAIL_PATH)
   async verifyEmail(
     @TypedQuery() query: IVerifyEmailQueryDto,
